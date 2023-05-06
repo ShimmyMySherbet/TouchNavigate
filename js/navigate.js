101558f22ac0a7d625146500f69550aa46890270
ShimmyMySherbet <shimmy@shimmymysherbet.com>
2023-05-06 16:12:52 +1000

    simplified script

--- js/navigate.js
+++ js/navigate.js
@@ -1,45 +1,25 @@
 var dragStartX = 0
 var dragStartY = 0
 
-function initTouchNavigation() {
-    document.body.addEventListener('touchstart', touchStart)
-    document.body.addEventListener('touchend', touchEnd)
-}
-
-function touchStart(event) {
+function onTouchStart(event) {
     dragStartX = event.changedTouches[0].clientX
     dragStartY = event.changedTouches[0].clientY
 }
 
-function touchEnd(event) {
-    var endX = event.changedTouches[0].clientX
+function onTouchEnd(event) {
+    var dragEndX = event.changedTouches[0].clientX
 
     // coordinate locations as a percentage of screen size
     var startXPercent = dragStartX / window.innerWidth
-    var endXPercent = endX / window.innerWidth
+    var endXPercent = dragEndX / window.innerWidth
 
-    if (
-        startXPercent <= 0.05 &&
-        endXPercent > startXPercent &&
-        endXPercent - startXPercent > 0.03
-    ) {
-        handleGoBack();
-    } else if (
-        startXPercent >= 0.95 &&
-        endXPercent < startXPercent &&
-        startXPercent - endXPercent > 0.03
-    ) {
-        handleGoForward();
+    // Tests if the drag coordinates are within back/forward bounds, and the distance is sufficient
+    if (startXPercent <= 0.05 && endXPercent > startXPercent && endXPercent - startXPercent > 0.03) {
+        history.back()
+    } else if (startXPercent >= 0.95 && endXPercent < startXPercent && startXPercent - endXPercent > 0.03) {
+        history.forward();
     }
 }
 
-function handleGoForward() {
-    history.forward();
-}
-
-function handleGoBack() {
-    history.back()
-}
-
-
-initTouchNavigation();
+document.body.addEventListener('touchstart', onTouchStart)
+document.body.addEventListener('touchend', onTouchEnd)
